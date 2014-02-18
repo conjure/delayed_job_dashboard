1f5551a78a41643c71d662c02ba6ddd51adec7e4
Ross Beale <rossbeale@gmail.com>
2014-02-18 16:12:06 +0000

    Added buttons to manage jobs

--- app/controllers/dj_dashboard/jobs_controller.rb
+++ app/controllers/dj_dashboard/jobs_controller.rb
@@ -2,13 +2,30 @@ class DjDashboard::JobsController < ::ActionController::Base
 
   layout "dj_dashboard/application"
 
+  before_filter :get_job, only: [:show, :delete]
+
   def index
     @jobs = Delayed::Job.all.order("run_at DESC")
   end
 
   def show
-    @job = Delayed::Job.find params[:id]
     @handler = YAML.load(@job.handler)
   end
+
+  def delete
+  	@job.destroy
+  	redirect_to jobs_path
+  end
+
+  def clear
+  	Delayed::Job.destroy_all
+  	redirect_to jobs_path
+  end
+
+  private
+
+  	def get_job
+    	@job = Delayed::Job.find params[:id]
+  	end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,10 @@
 DjDashboard::Engine.routes.draw do
   root controller: :jobs, action: :index
 
-  resources :jobs, only: [:index, :show]
+  resources :jobs, only: [:index, :show] do
+  	get 'clear', on: :collection, to: 'jobs#clear'
+  	get 'delete', on: :member, to: 'jobs#delete'
+  end
 
   get 'settings', controller: :settings, action: :show, as: :settings
 end
